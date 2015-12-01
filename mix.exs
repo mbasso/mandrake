b9961dc19f15e65d94edcf8556ece9e2450b007e
mbasso <mbasso96@gmail.com>
2015-12-01 21:35:52 +0100

    travis

--- mix.exs
+++ mix.exs
@@ -1,9 +1,11 @@
 defmodule Mandrake.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [app: :Mandrake,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -11,7 +13,7 @@ defmodule Mandrake.Mixfile do
      name: "Mandrake",
      docs: [source_ref: "v#{@version}", main: "Mandrake", logo: "logo.png"],
      source_url: "https://github.com/mbasso/mandrake",
-     homepage_url: "http://www.mandrake.teamturkeys.com",
+     package: [mantainers: ["Matteo Basso"], licenses: ["MIT"], links: %{"GitHub" => "https://github.com/mbasso/mandrake", "Docs" => "http://hexdocs.pm/mandrake" } ],
      description: """
      Mandrake is a functional programming library that bring something else magic in elixir.
      """]
@@ -35,6 +37,7 @@ defmodule Mandrake.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.11", only: :dev}]
+     {:ex_doc, "~> 0.11", only: :dev},
+     {:inch_ex, only: :docs}]
   end
 end
