e6abe5d7be8747c121df8bb5db608ed2f480f637
mbasso <mbasso96@gmail.com>
2015-12-01 23:58:34 +0100

    Fix package

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Mandrake.Mixfile do
      docs: [source_ref: "v#{@version}", main: "api-reference", logo: "logo.png"],
      source_url: "https://github.com/mbasso/mandrake",
      homepage_url: "https://github.com/mbasso/mandrake",
-     package: [maintainers: ["Matteo Basso"], licenses: ["MIT"], links: %{"GitHub" => "https://github.com/mbasso/mandrake", "Docs" => "http://hexdocs.pm/mandrake" } ],
+     package: package,
      description: """
      Mandrake is a functional programming library that bring something else magic in elixir.
      """]
@@ -41,4 +41,14 @@ defmodule Mandrake.Mixfile do
      {:ex_doc, "~> 0.11", only: :dev},
      {:inch_ex, only: :docs}]
   end
+
+  defp package do
+    [ 
+      name: "mandrake", 
+      maintainers: ["Matteo Basso"], 
+      licenses: ["MIT"], 
+      links: %{"GitHub" => "https://github.com/mbasso/mandrake", "Docs" => "http://hexdocs.pm/mandrake" }
+    ]
+  end
+
 end
