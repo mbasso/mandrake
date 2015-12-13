8c2d68245b522b341ba76641e639b3f740e6c181
mbasso <mbasso96@gmail.com>
2015-12-13 17:26:05 +0100

    Delete comments

--- mix.exs
+++ mix.exs
@@ -19,23 +19,11 @@ defmodule Mandrake.Mixfile do
      Mandrake is a functional programming library that bring something else magic in elixir.
      """]
   end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
